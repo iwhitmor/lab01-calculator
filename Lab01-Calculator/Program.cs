2ced00fb4e43c4a74253702c61ec861eb52b7a7f
Ian Whitmore <ianwhitmor@gmail.com>
2021-10-04 17:18:08 -0500

    create all methods and try, catch, finally in main method

--- Lab01-Calculator/Program.cs
+++ Lab01-Calculator/Program.cs
@@ -6,7 +6,43 @@ namespace Lab01_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                StartSequence();
+            }
+
+            catch (Exception)
+
+            {
+                Console.WriteLine("I'm sorry, but something has gone terribly wrong");
+            }
+
+            finally
+
+            {
+                Console.WriteLine("Let's Go!");
+            }
+            
+        }
+
+        static void StartSequence()
+        {
+            
+        }
+
+        static int[] Populate(int[] array)
+        { 
+            return array;
+        }
+
+        static int GetSum(int[] array)
+        {
+            return 1;
+        }
+
+        static int GetProduct(int[] array, int sum)
+        {
+            return 1;
         }
     }
 }
